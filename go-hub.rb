8aa9d97c012a644bcf87ba296be1ba20db9b0fee
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-23 11:41:18 +0800

    Brew formula update for go-hub version 0.2.16

--- go-hub.rb
+++ go-hub.rb
@@ -2,16 +2,16 @@
 class GoHub < Formula
   desc "A simple go cli to make github common actions simpler"
   homepage "https://github.com/hankxdev/go-hub"
-  version "0.2.12"
+  version "0.2.16"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/hankxdev/go-hub/releases/download/0.2.12/go-hub_0.2.12_Darwin_x86_64.tar.gz"
-    sha256 "0e8348998e7779242109200c93f7d2eb243dfd2f60670dbbb82d699ad92b1643"
+    url "https://github.com/hankxdev/go-hub/releases/download/0.2.16/go-hub_0.2.16_Darwin_x86_64.tar.gz"
+    sha256 "661e8ac9cf33217af6ad06a9533f7c6600fd991451d2f10cf80e492bf7611fed"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/hankxdev/go-hub/releases/download/0.2.12/go-hub_0.2.12_Linux_x86_64.tar.gz"
-      sha256 "7d9230243102e91e134bce55503080ba0a9ebad9bba783175b11aff7c5936c80"
+      url "https://github.com/hankxdev/go-hub/releases/download/0.2.16/go-hub_0.2.16_Linux_x86_64.tar.gz"
+      sha256 "958a9fd56cd0acf944001e69a2eb14ea9ee37d79544a84abeb1f23782e086b5d"
     end
   end
 
